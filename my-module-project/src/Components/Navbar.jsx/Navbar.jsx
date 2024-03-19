ac9100c74c43002dfac1832d3f20b2949067281e
Mridul5300 <mridulsarkar1157@gmail.com>
2024-03-19 12:29:33 +0600

    mebership chart ready

--- my-module-project/src/Components/Navbar.jsx/Navbar.jsx
+++ my-module-project/src/Components/Navbar.jsx/Navbar.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { CgMenuLeftAlt} from "react-icons/cg";
 import { RxCross1 } from "react-icons/rx";
+import Priceoption from "../Priceoption/Priceoption";
 
 const Navbar = () => {
         const [open, setOpen] = useState(false)
@@ -12,7 +13,7 @@ const Navbar = () => {
           { id: 5, path: '*', name: 'NotFound' }
         ];
 
-        const Priceoption =[
+        const Pricecaption =[
           
                {
                  "id": 1,
@@ -86,37 +87,53 @@ const Navbar = () => {
                    "Senior citizen discounts apply"
                  ]
                }
-             ]
-             
-        
-        
-
+             ]        
      return (
-          <nav>
+          <>
+               <nav>  
+                 <div className="flex justify-between">
+                    <div className="my-10 py-1 mx-4">
+                      <h2 className="text-2xl">Mr Treaders</h2>
+                    </div>
+                    <div>
+                    <div className="md:hidden p-10"  onClick={() => setOpen(!open)}>
 
-               <div className="md:hidden p-10"  onClick={() => setOpen(!open)}>
+{
+     open === true ? <RxCross1 className="text-3xl"></RxCross1> 
+     : <CgMenuLeftAlt className="text-3xl" ></CgMenuLeftAlt>
+     
+}
 
-                    {
-                         open === true ? <RxCross1 className="text-3xl"></RxCross1> 
-                         : <CgMenuLeftAlt className="text-3xl" ></CgMenuLeftAlt>
-                         
-                    }
-               
                </div>
-               <ul className={`md:flex absolute md:static md:my-12 px-12 duration-1000
+               <ul className={`md:flex md:justify-between absolute md:static md:my-12  duration-1000
                ${open ? 'top-20' : '-top-72' }
                `}>
+               
                {
-                    routes.map(route => <li className="mr-11" key={route.id}>
-                         <a href={route.path}>{route.name}</a>
-                    </li>)
+               routes.map(route => <li className="mr-11" key={route.id}>
+                    <a href={route.path}>{route.name}</a>
+               </li>)
                }
                </ul>
-
-               <main>
+                    </div>
                     
+                    </div>
+               </nav>
+               <main>
+                    <div className="my-4">
+                         <h2 className="text-5xl text-center">Best Price in The Town</h2>
+                    </div>
+                    <div className="grid md:grid-cols-2 gap-6 m-12">
+                        {
+                          
+                          Pricecaption.map(option => <Priceoption key={option} option={option}></Priceoption>)
+                        } 
+                    </div>
+
+                 
+
                </main>
-          </nav>
+          </>
      
      );
 };
---
+++ my-module-project/src/Components/Priceoption/Priceoption.jsx
@@ -0,0 +1,30 @@
+
+import PropTypes from 'prop-types';
+import { FaRegCircleCheck } from "react-icons/fa6";
+
+const Priceoption = ({option}) => {
+     const {name,price,features} =option;
+     return (
+          <div className='bg-amber-50 border-2 border-inherit p-4 py-4 flex-col rounded-xl shadow-lg'>
+              <h2>
+               <span className="text-4xl">{price}</span>
+               <span className="text-3xl">/mon</span>
+              </h2>
+              <h3 className='text-2xl mt-2'>{name}</h3>
+              <p className='pl-4 mt-2 flex-grow'>
+                   {
+                    features.map((feature, idx)=> 
+                         <h2 className='flex gap-2' key={idx}><span className='py-2 text-green-500 font-bold'><FaRegCircleCheck></FaRegCircleCheck></span>
+                    {feature}</h2>)
+                   }
+              </p>
+              <button className='btn bg-green-500 w-full text-2xl mt-2'>Buy Now</button>
+          </div>
+     );
+};
+
+Priceoption.propTypes = {
+     option:PropTypes.object
+};
+
+export default Priceoption;
